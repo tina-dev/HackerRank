ff71bd62022d25dc39ea963bfabeb9b83e2c82c1
Tina <tina.temishova@gmail.com>
2018-02-21 11:31:00 +0600

    Solution for JavaLoops2 is added.

---
+++ src/net/tina/HackerRank/Java/Introduction/P02_JavaLoops1.java
@@ -0,0 +1,15 @@
+package net.tina.HackerRank.Java.Introduction;
+
+public class P02_JavaLoops1 
+{
+	public static void main(String [] args)
+	{
+		int n = 2; 
+		for(int i = 1; i<=10; i++)
+		{
+			int result = n * i;
+			System.out.println(n + " * " + i + " = " + result);
+		}
+		
+	}
+}

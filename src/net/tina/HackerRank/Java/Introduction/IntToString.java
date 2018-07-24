6ca8839474fe85858500634f6057a11f93744acb
Tina <tina.temishova@gmail.com>
2018-07-24 20:04:44 +0600

    Solution for IntToString is added.

---
+++ src/net/tina/HackerRank/Java/Introduction/IntToString.java
@@ -0,0 +1,13 @@
+package net.tina.HackerRank.Java.Introduction;
+
+public class IntToString
+{
+	public static void main(String [] args)
+	{
+		int a = 5;
+	    System.out.println(Integer.toString(12));
+	    
+	    String s = String.valueOf(a);
+	    System.out.println(s);
+	}
+}

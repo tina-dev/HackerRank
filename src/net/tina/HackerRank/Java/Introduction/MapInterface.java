e43eef5c44a87f03f1abb2e9fc2bdb483115ea86
Tina <tina.temishova@gmail.com>
2019-11-11 00:36:50 -0800

    Begin

---
+++ src/net/tina/HackerRank/Java/Introduction/MapInterface.java
@@ -0,0 +1,24 @@
+package net.tina.HackerRank.Java.Introduction;
+
+import java.util.Scanner;
+
+public class MapInterface
+{
+    public static void main(String []args)
+    {
+        Scanner in = new Scanner(System.in);
+        int n=in.nextInt();
+        in.nextLine();
+        for(int i=0;i<n;i++)
+        {
+            String name=in.nextLine();
+            int phone=in.nextInt();
+            in.nextLine();
+        }
+        while(in.hasNext())
+        {
+            String s=in.nextLine();
+        }
+    }
+}
+

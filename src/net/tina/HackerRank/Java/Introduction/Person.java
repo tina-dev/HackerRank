69d89474ae74a05cdb1c15583be42555a6d415a4
Tina <tina.temishova@gmail.com>
2019-11-29 23:20:17 -0600

    Day 3 of Coding Challenge.

---
+++ src/net/tina/HackerRank/Java/Introduction/Person.java
@@ -0,0 +1,52 @@
+package net.tina.HackerRank.Java.Introduction;
+
+import java.util.Scanner;
+
+public class Person {
+    private int age;
+
+    public Person(int initialAge)
+    {
+        this.age = initialAge;
+    }
+
+    public void amIOld()
+    {
+        if(age < 13)
+        {
+            System.out.println("You are young");
+        }
+        else  if (age >= 13 || age <18)
+        {
+            System.out.println("You are a teenager");
+        }
+        else
+        {
+            System.out.println("You are old");
+        }
+    }
+
+    public void yearPasses()
+    {
+
+    }
+
+
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        for (int i = 0; i < T; i++)
+        {
+            int age = sc.nextInt();
+            Person p = new Person(age);
+            p.amIOld();
+            for (int j = 0; j < 3; j++)
+            {
+                p.yearPasses();
+            }
+            p.amIOld();
+            System.out.println();
+        }
+    }
+}

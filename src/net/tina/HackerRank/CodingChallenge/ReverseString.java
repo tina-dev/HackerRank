86070f40cfead29af5febef3c942014662263e46
tinatemishova <tina.temishova@gmail.com>
2020-05-10 18:27:49 -0500

    ReverseString solution is added..

---
+++ src/net/tina/HackerRank/CodingChallenge/ReverseString.java
@@ -0,0 +1,24 @@
+package net.tina.HackerRank.CodingChallenge;
+
+public class ReverseString {
+    public static void main(String [] args) {
+        String str = "String";
+        System.out.println(reverseWithStringBuilder(str));
+        System.out.println(revreseManually(str));
+
+    }
+    public static String reverseWithStringBuilder(String str) {
+        return new StringBuilder(str)
+                .reverse()
+                .toString();
+    }
+
+    private static String revreseManually(String str) {
+        StringBuilder sb = new StringBuilder();
+
+        for(int i = str.length() - 1; i>=0; i--) {
+            sb.append(str.charAt(i));
+        }
+        return sb.toString();
+    }
+}

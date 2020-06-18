ad51296d9a71c8369fa9332377262fe608899371
tinatemishova <tina.temishova@gmail.com>
2020-06-17 20:20:15 -0500

    Exception String to Integer..

---
+++ src/net/tina/HackerRank/Java/Introduction/ExceptionChallenge.java
@@ -0,0 +1,11 @@
+package net.tina.HackerRank.Java.Introduction;
+
+import java.util.Scanner;
+
+public class ExceptionChallenge {
+    public static void main(String [] args) {
+        Scanner scanner = new Scanner(System.in);
+        String S = scanner.next();
+
+    }
+}
